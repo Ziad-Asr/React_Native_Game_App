d5af3165046c879dd8f86147a9d4d31065a9fbe3
Ziad <ZiadElsayedAsr@gmail.com>
2024-02-17 11:15:04 +0200

    update

---
+++ components/PrimaryButton.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+function PrimaryButton({ children }) {
+  return (
+    <View>
+      <Text>{children}</Text>
+    </View>
+  );
+}
+
+export default PrimaryButton;
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,47 @@
+import { StyleSheet, View, TextInput } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+}
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 24,
+    marginTop: 100,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4, // Android (Shadow)
+
+    // ios shadow
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    textAlign: "center",
+  },
+});
